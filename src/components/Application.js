84bcb2429de0b2c38920d191ac77060d10be22a2
Natalie Sokolova <nataliesklv@MacBook-Pro.local>
2020-08-03 22:36:16 -0400

    lifted the state up to the Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 
 import React, { useState } from "react";
 
@@ -25,6 +25,8 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -37,8 +39,8 @@ export default function Application(props) {
 <nav className="sidebar__menu">
 <DayList
   days={days}
-  day={"Monday"}
-  setDay={day => console.log(day)}
+  day={day}
+  setDay={setDay}
 />
 </nav>
 <img
