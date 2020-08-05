feeab3769da6d53e39392f11577f9e861c214d6e
Natalie Sokolova <nataliesklv@MacBook-Pro.local>
2020-08-04 22:34:55 -0400

    setting up for displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header.jsx";
+import Show from "components/Appointment/Show.jsx";
+import Empty from "components/Appointment/Empty.jsx";
+
 
 export default function Appointment() {
   return (
