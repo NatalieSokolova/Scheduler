3d662d3b1eaa07878e41972ce26cf3fc82479692
Natalie Sokolova <nataliesklv@MacBook-Pro.local>
2020-08-06 17:28:20 -0400

    added functionality to go back to a previous mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,34 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
+  // sets the initial mode
   const [mode, setMode] = useState(initial);
-  return { mode };
+
+  const [history, setHistory] = useState([initial]);
+
+  // enables transitioning into a different mode
+  const transition = newMode => {
+    const newHistory = [...history, newMode]
+    setHistory(newHistory)
+
+    console.log("newHistory: ",newHistory)
+    setMode(newMode)
+    console.log("newMode: ", newMode)
+  }
+  // enables going back to the previous mode
+  const back = () => {
+    console.log("mode:", mode)
+    console.log("sliced: ", history.slice(0, history.length - 1))
+
+    setHistory(history.slice(0, history.length - 1))
+
+    console.log("finalHistory: ", history.slice(0, history.length - 1))
+    console.log("prev: ", history[history.length - 1])
+
+    setMode(history[history.length - 2])
+    
+  }
+
+  return {mode, transition, back};
 }
+
